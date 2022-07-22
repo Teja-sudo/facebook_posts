0a1ffdd3ace4d37fd94f34b18da2867cba87a8a7
Teja-Sudo <sanikommutejareddy@gmail.com>
2022-07-22 09:40:39 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import RouteTo from './Route';
 import * as Sentry from "@sentry/react";
 
 const httpLink = createHttpLink({
-  uri: process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+  uri: REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET || process.env.HASURA_GRAPHQL_ADMIN_SECRET,
 });
 const authLink = setContext((_, { headers }) => {
-  const token = REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET_TOKEN;
+  const token = REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET_TOKEN || process.env.HASURA_GRAPHQL_ADMIN_SECRET_TOKEN;
   return {
     headers: {
       ...headers,
@@ -21,7 +21,8 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
-console.log(REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET?11:0)
+console.log(REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET? 11:0)
+console.log(process.env.HASURA_GRAPHQL_ADMIN_SECRET? 22:00)
 
 function App() {
   return (
